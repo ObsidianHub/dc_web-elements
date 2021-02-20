7100238ec77e2cfc604343211efe81bfe4928121
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-20 13:01:48 +0200

    end this project

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -21,4 +29,41 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlighTag(randomTag);
+
+    setTimeout(() => {
+      unHighlighTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlighTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlighTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlighTag(tag) {
+  tag.classList.remove("highlight");
 }
