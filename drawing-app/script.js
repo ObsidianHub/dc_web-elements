e1be7b3ad6e4dd27273369bc793cfc4f9c6f7b73
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-24 00:39:09 +0200

    add draw circle and draw line methods

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -13,6 +13,22 @@ let color = "black";
 let x;
 let y;
 
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+}
+
 function updateSizeOnScreen() {
   sizeEL.innerText = size;
 }
@@ -36,3 +52,9 @@ decreaseBtn.addEventListener("click", () => {
 
   updateSizeOnScreen();
 });
+
+colorEl.addEventListener("change", (e) => (color = e.target.value));
+
+clearEl.addEventListener("click", () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
