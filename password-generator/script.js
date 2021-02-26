2bd4766f212262e735a9377ed92d41ec93021e00
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-26 16:13:18 +0200

    realize generate password function

--- password-generator/script.js
+++ password-generator/script.js
@@ -46,6 +46,29 @@ generateEl.addEventListener("click", () => {
   );
 });
 
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
