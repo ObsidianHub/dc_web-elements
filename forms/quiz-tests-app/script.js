c6b1ee201455398e6033c30ed0a7ff7ad2d6b536
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-10-07 23:39:46 +0300

    add quiz tests application

---
+++ forms/quiz-tests-app/script.js
@@ -0,0 +1,97 @@
+const quizData = [
+  {
+    question: 'Какой язык работает в браузере?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd',
+  },
+  {
+    question: 'Как расшифровывается CSS',
+    a: 'Central Style Sheets',
+    b: 'Cascading Style Sheets',
+    c: 'Cascading Simple Sheets',
+    d: 'Cars SUVs Sailboats',
+    correct: 'b',
+  },
+  {
+    question: 'Как расшифровывается HTML?',
+    a: 'Hypertext Markup Language',
+    b: 'Hypertext Markdown Language',
+    c: 'Hyperloop Machine Language',
+    d: 'Helicopters Terminals Motorboats Lamborginis',
+    correct: 'a',
+  },
+  {
+    question: 'В каком году JavaScript появился?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'b',
+  },
+];
+
+const quiz = document.getElementById('quiz'),
+  answerEls = document.querySelectorAll('.answer'),
+  questionEl = document.getElementById('question'),
+  a_text = document.getElementById('a_text'),
+  b_text = document.getElementById('b_text'),
+  c_text = document.getElementById('c_text'),
+  d_text = document.getElementById('d_text'),
+  submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+          <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+          <button onclick="location.reload()">Reload</button>
+      `;
+    }
+  }
+});
