54a8b99515a7071455b23a216cc2441994bb57ea
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-10-14 17:23:30 +0300

    some changes and position replace

--- effects/knowledge-timeline/script.js
+++ effects/knowledge-timeline/script.js
@@ -6,11 +6,8 @@ const isInViewport = (el) => {
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
-      (window.innerHeight ||
-        document.documentElement.clientHeight) &&
-    rect.right <=
-      (window.innerWidth ||
-        document.documentElement.clientWidth)
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 };
 
