0b6479866293592a67d10a1f2e20205d7bc7e9cb
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-10-31 23:55:42 +0200

    add script for end relax preloader, fix some html/css

---
+++ preloaders/relax-preloader/script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container'),
+    text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+relaxAnimation();
+
+function relaxAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(relaxAnimation, totalTime);
