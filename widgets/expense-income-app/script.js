669f2b3ae19fc130a3fc3cc45ed1199310c84aa2
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-11-05 14:34:40 +0200

    add form handler, generateID func, and begin addTransaction func

---
+++ widgets/expense-income-app/script.js
@@ -0,0 +1,41 @@
+const balance = document.getElementById("balance"),
+  money_plus = document.getElementById("money-plus"),
+  money_minus = document.getElementById("money-minus"),
+  list = document.getElementById("list"),
+  form = document.getElementById("form"),
+  text = document.getElementById("text"),
+  amount = document.getElementById("amount");
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert("Please add a text and amount");
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+
+    transactions.push(transaction);
+
+    text.value = "";
+    amount.value = "";
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+form.addEventListener("submit", addTransaction);
