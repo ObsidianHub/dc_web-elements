8a71bf639175f6fe7d8d319a525efc5d44d32f3b
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-11-05 13:27:41 +0200

    end songs-searcher app -> add prev-next btns and realize get more songs (decided the issue with CORS error)

--- API/songs-searcher-app/script.js
+++ API/songs-searcher-app/script.js
@@ -33,6 +33,31 @@ function showData(data) {
         .join("")}
     </ul>
   `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
 }
 
 // Get lyrics for song
