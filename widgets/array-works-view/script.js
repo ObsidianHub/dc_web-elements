a05b16300a978891d72ab70f10cdd23921ca24b1
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-12-04 12:48:33 +0200

    add functions for generate content into DOM

--- widgets/array-works-view/script.js
+++ widgets/array-works-view/script.js
@@ -24,6 +24,24 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+function addData(obj) {
+  data.push(obj);
+  updateDOM();
+}
+
+function updateDOM(providedData = data) {
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
 function doubleMoney() {}
 
 function sortByRichest() {}
