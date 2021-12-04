26a1b33fbd3e14166ccad15af16303309c98313f
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-12-04 16:10:41 +0200

    end script for infinity scroll

--- widgets/infinity-scroll-images/script.js
+++ widgets/infinity-scroll-images/script.js
@@ -10,6 +10,46 @@ let ready = false,
   photosArray = [],
   initialLoad = true;
 
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    initialLoad = false;
+  }
+}
+
+function setAttributes(el, attr) {
+  for (const key in attr) {
+    el.setAttribute(key, attr[key]);
+  }
+}
+
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener("load", imageLoaded);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 async function getPhotos() {
   try {
     const res = await fetch(apiUrl);
