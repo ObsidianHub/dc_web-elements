fbd15e33ffb57e2f6a21fc443fde19ae577d56b0
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-12-10 21:18:51 +0200

    end work with todo list, fix some issues with clearList etc (not optimized)

--- todo-lists/full-todo-list/script.js
+++ todo-lists/full-todo-list/script.js
@@ -1,8 +1,18 @@
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 let ul = document.querySelector(".list-group"),
+  emptyAlert = document.querySelector(".empty-alert"),
+  clear = document.querySelector(".clear-btn"),
   form = document.forms["addTodoItem"],
   inputText = form.elements["todoText"],
   notificationAlert = document.querySelector(".notification-alert");
+
+function checkLocalStorage() {
+  if (tasks.length) {
+    generateList(tasks);
+  } else {
+    checkList();
+  }
+}
 
 function generateId() {
   let id = "";
@@ -13,6 +23,14 @@ function generateId() {
     id += words[position];
   }
   return id;
+}
+
+function checkList() {
+  if (!ul.children.length) {
+    emptyAlert.style.display = "block";
+  } else {
+    emptyAlert.style.display = "none";
+  }
 }
 
 function generateList(tasksArray) {
@@ -30,6 +48,7 @@ function listTemplate(task) {
   li.className = "list-group-item d-flex align-items-center";
   li.setAttribute("data-id", task.id);
   let span = document.createElement("span");
+  span.className = "p-5-20";
   span.textContent = task.text;
 
   // create tag i fa-trash-alt
@@ -58,9 +77,81 @@ function addList(list) {
   };
   tasks.unshift(newTask);
   ul.insertAdjacentElement("afterbegin", listTemplate(newTask));
-  // add to LocalStorage
+
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  message({
+    text: "Task added success",
+    cssClass: "alert-success",
+    timeout: 2000,
+  });
 }
+
+function deleteListItem(id) {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === id) {
+      tasks.splice(i, 1);
+      break;
+    }
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  message({
+    text: "Task deleted success",
+    cssClass: "alert-danger",
+    timeout: 3000,
+  });
+}
+
+function editListItem(id, newValue) {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === id) {
+      tasks[i].text = newValue;
+      break;
+    }
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  message({
+    text: "Task updated success",
+    cssClass: "alert-warning",
+    timeout: 3000,
+  });
+}
+
+function message(settings) {
+  notificationAlert.classList.add(settings.cssClass);
+  notificationAlert.textContent = settings.text;
+  notificationAlert.classList.add("show");
+
+  setTimeout(() => {
+    notificationAlert.classList.remove("show");
+  }, settings.timeout);
+}
+
+ul.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete-item")) {
+    let parent = e.target.closest("li");
+    let id = parent.dataset.id;
+    // delete list item
+    deleteListItem(id);
+    parent.remove();
+  } else if (e.target.classList.contains("edit-item")) {
+    e.target.classList.toggle("fa-save");
+    let id = e.target.closest("li").dataset.id;
+    let span = e.target.closest("li").querySelector("span");
+
+    if (e.target.classList.contains("fa-save")) {
+      span.setAttribute("contenteditable", true);
+      span.focus();
+    } else {
+      span.setAttribute("contenteditable", false);
+      span.blur();
+      editListItem(id, span.textContent);
+    }
+  }
+  checkLocalStorage();
+});
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -71,6 +162,7 @@ form.addEventListener("submit", function (e) {
     inputText.classList.remove("is-invalid");
     addList(inputText.value);
     form.reset();
+    checkList();
   }
 });
 
@@ -80,4 +172,11 @@ inputText.addEventListener("keyup", function () {
   }
 });
 
+clear.addEventListener("click", () => {
+  clearList();
+  localStorage.setItem("tasks", JSON.stringify((tasks = [])));
+  checkLocalStorage();
+});
+
+checkLocalStorage();
 generateList(tasks);
