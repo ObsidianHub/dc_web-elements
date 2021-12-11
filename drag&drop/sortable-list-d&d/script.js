1ead7cb38afbc13b319c3f774c05cc788fd00a9e
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-12-11 11:55:47 +0200

    start write d&d sortable list -> write all basic funcs for drap-and-drop and create list render

---
+++ drag&drop/sortable-list-d&d/script.js
@@ -0,0 +1,65 @@
+const draggableList = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+const listItems = [];
+let dragStartIndex;
+
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
+
+createList();
+
+function createList() {
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+      <span class="number">${index + 1}</span>
+      <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines"></i>
+      </div>
+    `;
+
+      listItems.push(listItem);
+
+      draggableList.appendChild(listItem);
+    });
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+
+function dragEnter() {
+  this.classList.add("over");
+}
+
+function dragLeave() {
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
