c6a1cceb502dab2ff93389ff1a6c98705ae84e5c
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-12-12 23:35:44 +0200

    end project -> form validator

--- forms/easy-form-validator/script.js
+++ forms/easy-form-validator/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("form"),
   username = document.getElementById("username"),
   email = document.getElementById("email"),
   password = document.getElementById("password"),
-  password2 = document.getElementById("password2");
+  passwordConfirm = document.getElementById("passwordConfirm");
 
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -16,8 +16,58 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
+  checkRequired([username, email, password, passwordConfirm]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, passwordConfirm);
 });
