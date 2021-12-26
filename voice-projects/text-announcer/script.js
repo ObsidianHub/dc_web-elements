0281d8841ff0a6c228019c950d768dcc39d2d1cf
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-12-26 14:12:15 +0200

    end the script for text annoucer application

--- voice-projects/text-announcer/script.js
+++ voice-projects/text-announcer/script.js
@@ -7,65 +7,45 @@ const main = document.querySelector('main'),
 
 const data = [
   {
-    image: './img/drink.jpg',
-    text: "I'm Thirsty",
+    image: './img/bmw.jpg',
+    text: 'There is I8 White Car Concept',
   },
   {
-    image: './img/food.jpg',
-    text: "I'm Hungry",
+    image: './img/tech.jpg',
+    text: 'Cloud technology now is our future',
   },
   {
-    image: './img/tired.jpg',
-    text: "I'm Tired",
+    image: './img/engineer.jpg',
+    text: "Engineer's vibe today",
   },
   {
-    image: './img/hurt.jpg',
-    text: "I'm Hurt",
+    image: './img/qpc.jpg',
+    text: 'Quantum dimensions as our future',
   },
   {
-    image: './img/happy.jpg',
-    text: "I'm Happy",
+    image: './img/dark.jpg',
+    text: 'Dark deep internet has danger',
   },
   {
-    image: './img/angry.jpg',
-    text: "I'm Angry",
+    image: './img/it.jpg',
+    text: 'Information Technology for e-commerce',
   },
   {
-    image: './img/sad.jpg',
-    text: "I'm Sad",
+    image: './img/basketball.jpg',
+    text: 'Take up the sport to live long and healthy',
   },
   {
-    image: './img/scared.jpg',
-    text: "I'm Scared",
-  },
-  {
-    image: './img/outside.jpg',
-    text: 'I Want To Go Outside',
-  },
-  {
-    image: './img/home.jpg',
-    text: 'I Want To Go Home',
-  },
-  {
-    image: './img/school.jpg',
-    text: 'I Want To Go To School',
-  },
-  {
-    image: './img/grandma.jpg',
-    text: 'I Want To Go To Grandmas',
+    image: './img/nature.jpg',
+    text: 'Nature created the best pictures',
   },
 ];
 
 data.forEach(createBox);
 
-// Create speech boxes
 function createBox(item) {
   const box = document.createElement('div');
-
   const { image, text } = item;
-
   box.classList.add('box');
-
   box.innerHTML = `
     <img src="${image}" alt="${text}" />
     <p class="info">${text}</p>
@@ -75,10 +55,57 @@ function createBox(item) {
     setTextMessage(text);
     speakText();
 
-    // Add active effect
     box.classList.add('active');
     setTimeout(() => box.classList.remove('active'), 800);
   });
 
   main.appendChild(box);
 }
+
+const message = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement('option');
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+function setTextMessage(text) {
+  message.text = text;
+}
+
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show')
+);
+
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show')
+);
+
+voicesSelect.addEventListener('change', setVoice);
+
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
